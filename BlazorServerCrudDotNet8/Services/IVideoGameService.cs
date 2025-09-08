7ebf7be49fc7fe36734acee496016b8ae4c401bf
kokonaing <kokonai1827@gmail.com>
2025-09-08 09:19:59 +0630

    feat: implement CRUD operation

--- BlazorServerCrudDotNet8/Services/IVideoGameService.cs
+++ BlazorServerCrudDotNet8/Services/IVideoGameService.cs
@@ -5,4 +5,8 @@ namespace BlazorServerCrudDotNet8.Services;
 public interface IVideoGameService
 {
     Task<List<VideoGame>> GetAllVideoGamesAsync();
+    Task<VideoGame> GetVideoGameByIdAsync(int id);
+    Task UpdateVideoGameAsync(VideoGame videoGame, int id);
+    Task AddGameAsync(VideoGame videoGame);
+    Task DeleteGameAsync(int id);
 }
--- BlazorServerCrudDotNet8/Services/VedioGameService.cs
+++ BlazorServerCrudDotNet8/Services/VedioGameService.cs
@@ -15,9 +15,42 @@ public class VedioGameService : IVideoGameService
         _context = context;
     }
 
+    public async Task AddGameAsync(VideoGame videoGame)
+    {
+        _context.VideoGames.Add(videoGame);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteGameAsync(int id)
+    {
+        var DbGame = await _context.VideoGames.FindAsync(id);
+        if (DbGame != null)
+        {
+            _context.Remove(DbGame);
+            await _context.SaveChangesAsync();
+        }
+    }
+
     public async Task<List<VideoGame>> GetAllVideoGamesAsync()
     {
         var reasult = await _context.VideoGames.ToListAsync();
         return reasult;
+    }
+
+    public async Task<VideoGame> GetVideoGameByIdAsync(int id)
+    {
+        return await _context.VideoGames.FindAsync(id);
+    }
+
+    public async Task UpdateVideoGameAsync(VideoGame videoGame, int id)
+    {
+        var dbGame = await _context.VideoGames.FindAsync(id);
+        if (dbGame != null)
+        {
+            dbGame.Title = videoGame.Title;
+            dbGame.Publisher = videoGame.Publisher;
+            dbGame.ReleaseYear = videoGame.ReleaseYear;
+            await _context.SaveChangesAsync();
+        }
     }
 }
